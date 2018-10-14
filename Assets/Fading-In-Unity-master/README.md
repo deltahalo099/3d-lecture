48c532dbea367a34e4e5f4b2e059f07919694e4b
Olya <thebestladyfox@gmail.com>
2018-10-14 09:15:35 -0400

    the beginning

---
+++ Assets/Fading-In-Unity-master/README.md
@@ -0,0 +1,6 @@
+# Polybrush
+Project files for my tutorial on fading between scenes in Unity.
+
+Check out my [YouTube Channel](http://youtube.com/brackeys) for more tutorials.
+
+Everything is free to use, also commercially (public domain).
---
+++ Assets/Swap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swap : MonoBehaviour {
+
+	public GameObject openBook;
+	public GameObject closedBook;
+
+	void OnTriggerEnter (Collider collider)
+	{
+		Debug.Log ("Object is within trigger");
+		closedBook.SetActive(false);
+		openBook.SetActive(true);
+
+	}
+	
+}
